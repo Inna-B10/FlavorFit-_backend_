a38a35d2f65bd95ca2b2b4cfd434133b0a1cbf60
Inna-B10 <inna.b10.kodehode@gmail.com>
2026-01-29 13:33:05 +0100

    feat: add GqlAuthGuard and GqlAdminGuard for role-based access control

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,14 @@
+import { applyDecorators, UseGuards } from '@nestjs/common'
+import { Role } from 'prisma/generated/prisma/enums'
+import { GqlAdminGuard } from '../guards/admin.guard'
+import { GqlAuthGuard } from '../guards/auth.guard'
+
+export const Auth = (role: Role = Role.USER) => {
+	if (role === Role.ADMIN) {
+		return applyDecorators(UseGuards(GqlAuthGuard, GqlAdminGuard))
+	}
+	return applyDecorators(UseGuards(GqlAuthGuard))
+}
+
+// example:
+// ex. Auth(_,subscription)
---
+++ src/auth/guards/admin.guard.ts
@@ -0,0 +1,19 @@
+import { ForbiddenError } from '@nestjs/apollo'
+import { CanActivate, ExecutionContext } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { GqlExecutionContext } from '@nestjs/graphql'
+import type { TRequestWithUser } from '../auth.interface'
+
+export class GqlAdminGuard implements CanActivate {
+	constructor(private readonly reflector: Reflector) {}
+	canActivate(context: ExecutionContext): boolean {
+		const ctx = GqlExecutionContext.create(context)
+		const user = ctx.getContext<{ req: TRequestWithUser }>().req.user
+
+		if (user?.role !== 'ADMIN') {
+			throw new ForbiddenError("You don't have permission to access this resource")
+		}
+
+		return true
+	}
+}
