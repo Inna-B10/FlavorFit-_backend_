7f5f73f296a69bfdba1f934015ae10f2993da190
Inna-B10 <inna.b10.kodehode@gmail.com>
2026-02-06 23:02:13 +0100

    fixed imports

--- src/recipes/reactions/reactions.module.ts
+++ src/recipes/reactions/reactions.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ReactionsService } from './reactions.service';
-import { ReactionsResolver } from './reactions.resolver';
+import { Module } from '@nestjs/common'
+import { PrismaModule } from 'src/prisma/prisma.module'
+import { ReactionsResolver } from './reactions.resolver'
+import { ReactionsService } from './reactions.service'
 
 @Module({
-  providers: [ReactionsResolver, ReactionsService],
+	providers: [ReactionsResolver, ReactionsService],
+	imports: [PrismaModule]
 })
 export class ReactionsModule {}
